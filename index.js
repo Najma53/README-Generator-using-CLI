46240fbc45e385abe8aa62366c2a350332030e5b
Najma53 <najma.alibhai@gmail.com>
2023-01-27 11:29:08 +0000

    added commented out notes to index.js from the README provided for assignment

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
 
+// When a user enters the project title then it is displayed as the title of the README
+//  When a user enters a description then this information is added to the sections of the README
+// When a user eners installation instructions then this information is added to the sections of the README
+// When a user eners usage information  then this information is added to the sections of the README
+// When a user eners contribution guidelines then this information is added to the sections of the README
+//  When a user eners and test instructions then this information is added to the sections of the README 
+
+
+
+
 ];
 
 // function to write README file
