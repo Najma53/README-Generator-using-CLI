f434e526ebc8b0d1c3919c670eefb3ccb7506589
Najma53 <najma.alibhai@gmail.com>
2023-01-29 11:33:19 +0000

    added questions to be asked on index.js file

--- index.js
+++ index.js
@@ -7,12 +7,66 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
 
 // When a user enters the project title then it is displayed as the title of the README
+{
+    type: "input",
+    message: "Please enter the title of your project",
+    name: "title",
+},
 //  When a user enters a description then this information is added to the sections of the README
-// When a user eners installation instructions then this information is added to the sections of the README
-// When a user eners usage information  then this information is added to the sections of the README
-// When a user eners contribution guidelines then this information is added to the sections of the README
-//  When a user eners and test instructions then this information is added to the sections of the README 
+{
+    type: "input",
+    message: "Please enter a brief description of your project",
+    name: "description",
+},
 
+// When a user eners installation instructions then this information is added to the sections of the README
+{
+    type: "input",
+    message: "Please give a brief description on how to install your project",
+    name: "installation",
+    default: "N/A"
+},
+// When a user enters usage information  then this information is added to the sections of the README
+
+{
+    type: "input",
+    message: "Please enter a brief description on how to use the application",
+    name: "usage",
+}, 
+// When a user eners contribution guidelines then this information is added to the sections of the README
+
+{
+    type: "input",
+    message: "Please give a brief description on how to contribute to the project",
+    name: "contributing",
+},
+//  When a user enters and test instructions then this information is added to the sections of the README 
+{
+    type: "input",
+    message: "Please give a brief description on how to update for test",
+    name: "tests"
+},
+//  When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled 
+{
+    type: "list",
+    message: "Please choose your licence",
+    name: "license",
+    choices: ["MIT", "Apache", "GPLv2"]
+}, 
+//  When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
+{
+    type: "input",
+    message: "Please enter your GitHub username",
+    name: "github"
+},
+
+//  When a user enters their email address then this is added to the section of the README entitled Questions, with instructions on how to reach them with additional questions
+{
+    type: "input",
+    message: "Please enter your email address where users and contributors can reach you with addtional questions",
+    name: "email"
+},
+//  When a user clicks on the links in the **Table of Contents** then they are taken to the corresponding section of the README
 
 
 
